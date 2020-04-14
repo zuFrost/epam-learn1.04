1a98902533d2d704943383b144772dbe5078000a
Alex Frost <zufrost.a@gmail.com>
2020-04-14 12:34:10 +0300

    Create LearnMain Class

---
+++ src/com/zufrost/learn/LearnMain.java
@@ -0,0 +1,20 @@
+package com.zufrost.learn;
+
+public class LearnMain {
+    public static void main(String[] args) {
+        System.out.println("hello Java");
+        System.out.println(42);
+        System.out.println("\n\tjava " + 11);
+
+        int i = 10;
+        if(i < 10) {
+            System.out.print(1);
+        } else if (i <= 10) {
+            System.out.print(2);
+        }
+        System.exit(i);
+        System.out.print(3);
+
+
+    }
+}
